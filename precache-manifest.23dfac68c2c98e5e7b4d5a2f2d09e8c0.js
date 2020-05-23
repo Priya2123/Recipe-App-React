29d42213d3dcc67340a00279b0ad4b0b3cc6fab9
Priya Aggarwal <aggarwalpriya1121@gmail.com>
2020-05-23 23:38:31 +0530

    Updates

---
+++ precache-manifest.23dfac68c2c98e5e7b4d5a2f2d09e8c0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3964b1e5e7e1fbe17a6938bffcb3255",
+    "url": "/Recipe-React-App/index.html"
+  },
+  {
+    "revision": "fbaf1c5d7df6be47b885",
+    "url": "/Recipe-React-App/static/css/main.9aefe8e0.chunk.css"
+  },
+  {
+    "revision": "0a3b40a727f3a73e40a4",
+    "url": "/Recipe-React-App/static/js/2.8c9ef27d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Recipe-React-App/static/js/2.8c9ef27d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fbaf1c5d7df6be47b885",
+    "url": "/Recipe-React-App/static/js/main.c740e2db.chunk.js"
+  },
+  {
+    "revision": "3bd8e2a695a2d649fcfe",
+    "url": "/Recipe-React-App/static/js/runtime-main.d2b7a37f.js"
+  },
+  {
+    "revision": "57b5515fec5f63b0b41c8fb16dc4183f",
+    "url": "/Recipe-React-App/static/media/1.57b5515f.jpeg"
+  }
+]);
