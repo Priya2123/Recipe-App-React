23cf9856b32f715785d0cd5c3ca76bf8dad98887
Priya Aggarwal <aggarwalpriya1121@gmail.com>
2020-10-18 03:15:02 +0530

    animations

--- src/App.js
+++ src/App.js
@@ -1,57 +1,83 @@
-import React, {useEffect, useState} from 'react';
-import Title from './Title';
-import './App.css';
-import Recipe from './Recipe';
+import React, { useEffect, useState } from "react";
+import Title from "./Title";
+import "./App.css";
+import { motion } from "framer-motion";
+import Recipe from "./Recipe";
 
+
+const buttonVariants = {
+  hover: {
+    scale: 1.1,
+    transition: {
+      //yoyo allows us to repeat animations - here any number is the no. of keyframes(from original value)
+      yoyo: Infinity,
+      duration: 0.8,
+    },
+  },
+};
 
 const App = () => {
-  
-  const APP_ID = 'b98e2152';
-  const APP_KEY = '9e68443d9f576a1ccb2d197447de741b';
-  
-const [recipes, setRecipes] = useState([]);
-const [search, setSearch] = useState('');
-const [query, setQuery] = useState('chicken');
-// const [counter, setCounter] = useState(0);
+  const APP_ID = "b98e2152";
+  const APP_KEY = "9e68443d9f576a1ccb2d197447de741b";
 
-useEffect(() => {
-  getRecipes();
-}, [query]);
+  const [recipes, setRecipes] = useState([]);
+  const [search, setSearch] = useState("");
+  const [query, setQuery] = useState("chicken");
+  // const [counter, setCounter] = useState(0);
 
-const getRecipes = async () => {
-  const response = await fetch(`https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`);
-  const data = await response.json();
-  // console.log(data);
-  setRecipes(data.hits);
-}
+  useEffect(() => {
+    getRecipes();
+  }, [query]);
 
-const updateSearch = e => {
-  setSearch(e.target.value);
-}
-const getSearch = e => {
-  e.preventDefault();
-  setQuery(search);
-  setSearch('');
-}
+  const getRecipes = async () => {
+    const response = await fetch(
+      `https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`
+    );
+    const data = await response.json();
+    // console.log(data);
+    setRecipes(data.hits);
+  };
 
-  return(
-    <div className="App">
-     
+  const updateSearch = (e) => {
+    setSearch(e.target.value);
+  };
+  const getSearch = (e) => {
+    e.preventDefault();
+    setQuery(search);
+    setSearch("");
+  };
+
+  return (
+  <motion.div className="App">
       <div className="image-r">
-       <Title />
-      </div>  
-     <form onSubmit={getSearch} className="search-form">
-     
-       <input className="search-bar" type="text" value={search} onChange={updateSearch} />
-       <button className="search-button" type="submit">Search</button>
-     </form>
-     <div className="recipes">
-     {recipes.map(recipe => (
-       <Recipe title={recipe.recipe.label} calories={recipe.recipe.calories} image={recipe.recipe.image} ingredients={recipe.recipe.ingredients}/>
-     ))};
-     </div>
-     </div>
+        <Title />
+      </div>
+      <form onSubmit={getSearch} className="search-form">
+        <input
+          className="search-bar"
+          type="text"
+          value={search}
+          onChange={updateSearch}
+        />
+        <motion.button  variants={buttonVariants}
+          whileHover="hover"
+          className="search-button" type="submit">
+          Search
+        </motion.button>
+      </form>
+      <div className="recipes">
+        {recipes.map((recipe) => (
+          <Recipe
+            title={recipe.recipe.label}
+            calories={recipe.recipe.calories}
+            image={recipe.recipe.image}
+            ingredients={recipe.recipe.ingredients}
+          />
+        ))}
+        ;
+      </div>
+    </motion.div>
   );
-} 
+};
 
 export default App;
--- src/Title.js
+++ src/Title.js
@@ -1,26 +1,43 @@
-import React from 'react';
-import {Spring} from 'react-spring/renderprops';
-import style from './title.css';
-
+import React from "react";
+import { Spring } from "react-spring/renderprops";
+import style from "./title.css";
+import { motion } from "framer-motion";
+import TextLoop from "react-text-loop";
 
 export default function Title() {
-    return (
-       
-            <Spring
-            from={{opacity: 0}}
-            to={{opacity: 1}}
-            config={{duration: 1000, delay: 1000}}
-            >
-            {props => (
-                <div style={props}>
-                <div style={style.p}>
-                <p><span style={{marginLeft:'530px'}}>Let's</span><br/><span style={{fontSize:'120px', letterSpacing:'17px', marginLeft:'500px'}}>COOK</span></p>   
-                </div>
-                </div>
-            )}
-
-
-            </Spring>
-       
-    );
+  return (
+    <Spring
+      from={{ opacity: 0 }}
+      to={{ opacity: 1 }}
+      config={{ duration: 1000, delay: 1000 }}
+    >
+      {(props) => (
+        <div style={props}>
+          <div style={style.p}>
+            <p>
+              <span style={{ marginLeft: "530px" }}>Let's</span>
+              <br />
+              <span
+                style={{
+                  fontSize: "120px",
+                  letterSpacing: "17px",
+                  marginLeft: "500px",
+                }}
+              >
+                COOK{" "}
+              </span>
+              <span>
+              <TextLoop style={{fontSize: '8px'}} interval={200}>
+            <span>Pizza</span>
+            <span>Burger</span>
+            <span>Fries</span>
+            <span>Chicken</span>
+          </TextLoop>
+              </span>
+            </p>
+          </div>
+        </div>
+      )}
+    </Spring>
+  );
 }
